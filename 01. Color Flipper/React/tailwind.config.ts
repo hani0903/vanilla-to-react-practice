d4316bc3ee607e788c1131bc695b40517e9fb0aa
조하은 <jhe93@naver.com>
2025-06-24 20:02:23 +0900

    chore: initialize project with Vite + TypeScript

---
+++ 01. Color Flipper/React/tailwind.config.ts
@@ -0,0 +1,57 @@
+// import type { Config } from 'tailwindcss';
+
+// const config: Config = {
+//     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+//     safelist: ['font-primary', 'font-secondary'],
+//     theme: {
+//         extend: {
+//             colors: {
+//                 primary: {
+//                     1: 'hsl(205, 86%, 17%)',
+//                     2: 'hsl(205, 77%, 27%)',
+//                     3: 'hsl(205, 72%, 37%)',
+//                     4: 'hsl(205, 63%, 48%)',
+//                     5: 'hsl(205, 78%, 60%)',
+//                     6: 'hsl(205, 89%, 70%)',
+//                     7: 'hsl(205, 90%, 76%)',
+//                     8: 'hsl(205, 86%, 81%)',
+//                     9: 'hsl(205, 90%, 88%)',
+//                     10: 'hsl(205, 100%, 96%)',
+//                 },
+//                 grey: {
+//                     1: 'hsl(209, 61%, 16%)',
+//                     2: 'hsl(211, 39%, 23%)',
+//                     3: 'hsl(209, 34%, 30%)',
+//                     4: 'hsl(209, 28%, 39%)',
+//                     5: 'hsl(210, 22%, 49%)',
+//                     6: 'hsl(209, 23%, 60%)',
+//                     7: 'hsl(211, 27%, 70%)',
+//                     8: 'hsl(210, 31%, 80%)',
+//                     9: 'hsl(212, 33%, 89%)',
+//                     10: 'hsl(210, 36%, 96%)',
+//                 },
+//             },
+//             fontFamily: {
+//                 primary: ['Roboto', 'sans-serif'],
+//                 secondary: ['Open Sans', 'sans-serif'],
+//             },
+//             letterSpacing: {
+//                 spacing: '0.1rem',
+//             },
+//             boxShadow: {
+//                 light: '0 5px 15px rgba(0, 0, 0, 0.1)',
+//                 dark: '0 5px 15px rgba(0, 0, 0, 0.2)',
+//             },
+//             maxWidth: {
+//                 screen: '1170px',
+//                 fixed: '620px',
+//             },
+//             transitionProperty: {
+//                 default: 'all',
+//             },
+//         },
+//     },
+//     plugins: [],
+// };
+
+// export default config;
