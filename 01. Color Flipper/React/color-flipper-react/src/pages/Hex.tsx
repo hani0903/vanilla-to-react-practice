7890902c56dd204013dd5829079dfd4bc4c91b73
조하은 <jhe93@naver.com>
2025-06-25 13:37:10 +0900

    refactor: use local state in Rgb
    - Replace context-based bgColor management with local useState in Rgb component
    - Wrap content in `<main>`

--- 01. Color Flipper/React/color-flipper-react/src/pages/Hex.tsx
+++ 01. Color Flipper/React/color-flipper-react/src/pages/Hex.tsx
@@ -1,14 +1,9 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { BgColorContext } from './Root';
+import { useState } from 'react';
 
 const HEX = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
 
 function Hex() {
-    const { bgColor, setBgColor } = useContext(BgColorContext);
-
-    useEffect(() => {
-        setBgColor('#f1f5f8');
-    }, []);
+    const [bgColor, setBgColor] = useState<string>('#f1f5f8');
 
     function handleChangeBgWithHex() {
         let newBg = '#';
@@ -20,17 +15,19 @@ function Hex() {
         setBgColor(newBg);
     }
     return (
-        <div className={`text-center`}>
-            <h2 className="font-primary bg-black text-white px-4 py-6 mb-10 font-bold rounded-md">
-                background color: <span className="text-primary-5">{bgColor}</span>
-            </h2>
-            <button
-                onClick={handleChangeBgWithHex}
-                className="px-5 py-3 font-primary uppercase text-black tracking-spacing-default inline-block font-bold transition-default border-2 cursor-pointer shadow-button rounded-default text-base"
-            >
-                click me
-            </button>
-        </div>
+        <main className={`flex-1 grid place-content-center`} style={{ backgroundColor: bgColor }}>
+            <div className={`text-center`}>
+                <h2 className="font-primary bg-black text-white px-4 py-6 mb-10 font-bold rounded-md">
+                    background color: <span className="text-primary-5">{bgColor}</span>
+                </h2>
+                <button
+                    onClick={handleChangeBgWithHex}
+                    className="px-5 py-3 font-primary uppercase text-black tracking-spacing-default inline-block font-bold transition-default border-2 cursor-pointer shadow-button rounded-default text-base"
+                >
+                    click me
+                </button>
+            </div>
+        </main>
     );
 }
 
