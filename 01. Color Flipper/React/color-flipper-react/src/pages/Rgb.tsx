3abd24b3fdff2ef1100aed8442fb99bfed27aca9
조하은 <jhe93@naver.com>
2025-06-25 13:37:49 +0900

    refactor: use local state in Hex
    - Replace context-based bgColor management with local useState in Hex component
    - Wrap content in `<main>`

--- 01. Color Flipper/React/color-flipper-react/src/pages/Rgb.tsx
+++ 01. Color Flipper/React/color-flipper-react/src/pages/Rgb.tsx
@@ -1,12 +1,7 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { BgColorContext } from './Root';
+import { useState } from 'react';
 
 function Rgb() {
-    const { bgColor, setBgColor } = useContext(BgColorContext);
-
-    useEffect(() => {
-        setBgColor('rgb(241, 245, 248)');
-    }, []);
+    const [bgColor, setBgColor] = useState<string>('rgb(241, 245, 248)');
 
     function handleChangeBgWithRgb() {
         const red = randomNumber();
@@ -15,17 +10,19 @@ function Rgb() {
         setBgColor(`rgb(${red}, ${green}, ${blue})`);
     }
     return (
-        <div className={`text-center`}>
-            <h2 className="font-primary bg-black text-white px-4 py-6 mb-10 font-bold rounded-md">
-                background color: <span className="text-primary-5">{bgColor}</span>
-            </h2>
-            <button
-                onClick={handleChangeBgWithRgb}
-                className="px-5 py-3 font-primary uppercase text-black tracking-spacing-default inline-block font-bold transition-default border-2 cursor-pointer shadow-button rounded-default text-base"
-            >
-                click me
-            </button>
-        </div>
+        <main className={`flex-1 grid place-content-center`} style={{ backgroundColor: bgColor }}>
+            <div className={`text-center`}>
+                <h2 className="font-primary bg-black text-white px-4 py-6 mb-10 font-bold rounded-md">
+                    background color: <span className="text-primary-5">{bgColor}</span>
+                </h2>
+                <button
+                    onClick={handleChangeBgWithRgb}
+                    className="px-5 py-3 font-primary uppercase text-black tracking-spacing-default inline-block font-bold transition-default border-2 cursor-pointer shadow-button rounded-default text-base"
+                >
+                    click me
+                </button>
+            </div>
+        </main>
     );
 }
 
